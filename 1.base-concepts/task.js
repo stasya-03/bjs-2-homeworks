9a08f60097ce6d27c92059f5b00aeaab21736902
Anastasia <nastya.il03@mail.ru>
2025-11-17 23:36:06 +0400

    Add solutions of tasks 1 and 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,49 @@
 "use strict"
+
+// Задача №1
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  }
+
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    return arr;
+  }
 }
 
+// Задача №2
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+  countMonths = Number(countMonths);
+
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
   
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+
+  if (S <= 0) {
+    return 0; 
+  }
+
+  let monthlyPayment = S * (P + P / (Math.pow(1 + P, countMonths) - 1));
+  let totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
